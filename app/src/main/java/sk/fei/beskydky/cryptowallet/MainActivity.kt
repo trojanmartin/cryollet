e8fd1723b65ffbc135040a31f85a0f2d893b3b09
Martin <martin.trojan.372@gmail.com>
2021-11-11 13:55:41 +0100

    project initialization

---
+++ app/src/main/java/sk/fei/beskydky/cryptowallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package sk.fei.beskydky.cryptowallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
