88ec153d51dc2a7833891b2e22e30ee45d64e303
Martin <martin.trojan.372@gmail.com>
2021-11-29 00:16:56 +0100

    feat: graph in progress

--- app/src/main/java/sk/fei/beskydky/cryollet/home/HomeFragment.kt
+++ app/src/main/java/sk/fei/beskydky/cryollet/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package sk.fei.beskydky.cryollet.home
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import sk.fei.beskydky.cryollet.R
 import sk.fei.beskydky.cryollet.databinding.HomeFragmentBinding
 import sk.fei.beskydky.cryollet.home.requestpayment.RequestPaymentFragment
@@ -44,6 +50,10 @@ class HomeFragment : Fragment() {
                 viewModel.onSendPaymentFinished()
             }
         })
+
+
+
+        setUpGraph(binding,  viewModel)
         binding.viewModel = viewModel
         binding.root.setHideKeyboardOnClick(this)
         return binding.root
@@ -52,5 +62,47 @@ class HomeFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         myContext = context as FragmentActivity
+    }
+
+    fun setUpGraph(binding: HomeFragmentBinding, viewModel: HomeViewModel){
+        var pieChart = binding.transactionGraph
+        val dataEntries = ArrayList<PieEntry>()
+        dataEntries.add(PieEntry(72f, "Android"))
+        dataEntries.add(PieEntry(26f, "Ios"))
+        dataEntries.add(PieEntry(2f, "Other"))
+
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(Color.parseColor("#373A4D"))
+        colors.add(Color.parseColor("#8F92A9"))
+        colors.add(Color.parseColor("#F5F5FA"))
+
+        val dataSet = PieDataSet(dataEntries, "")
+        val data = PieData(dataSet)
+
+        // In Percentage
+        data.setValueFormatter(PercentFormatter())
+        dataSet.sliceSpace = 3f
+        dataSet.colors = colors
+        pieChart.data = data
+        data.setValueTextSize(15f)
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+       // pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+        pieChart.setDrawEntryLabels(false)
+        pieChart.setDrawCenterText(false)
+        pieChart.setDrawMarkers(false)
+
+        //create hole in center
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+        pieChart.isDrawHoleEnabled = false
+
+//        pieChart.setDrawCenterText()
+//        pieChart.centerText = "Mobile OS Market share"
+
+        pieChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        pieChart.legend.verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+
+        pieChart.invalidate()
     }
 }
