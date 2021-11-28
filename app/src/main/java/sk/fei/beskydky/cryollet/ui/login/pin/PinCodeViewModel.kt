6b0c656d13b7de745db05c7a5883648847f23f3b
japubek <jakubtrstensky@gmail.com>
2021-11-28 13:53:23 +0100

    add pin fragment

---
+++ app/src/main/java/sk/fei/beskydky/cryollet/ui/login/pin/PinCodeViewModel.kt
@@ -0,0 +1,31 @@
+package sk.fei.beskydky.cryollet.ui.login.pin
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PinCodeViewModel(publicKey: String) : ViewModel() {
+    private val _ifUserHaveAlreadyPin = MutableLiveData<Boolean>()
+    val ifUserHaveAlreadyPin: LiveData<Boolean>
+        get() = _ifUserHaveAlreadyPin
+
+    var key = publicKey
+
+    //    init {
+//        var user = db.getUser(key)
+//        checkIfPinSet(user)
+//    }
+//
+    fun getUserPIN(): String {
+        // TODO: Resource get PIN from DB
+        return "1234"
+    }
+//
+//    fun checkIfPinSet(user: User) {
+//        // TODO: Get user by his public key
+//        // User user = db.getUser(publicKey)
+//        _ifUserHaveAlreadyPin.value = user != null
+//    }
+
+
+}
